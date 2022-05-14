5ee33904a3041997a33bc26a2c1cf0888a88161b
Hudson Gustavo <97410665+hudsonziin@users.noreply.github.com>
2022-05-14 01:15:23 -0300

    Delete aulas_JS directory

--- aulas_JS/organizacao.js
+++
@@ -1,7 +0,0 @@
-console.log("Sentença de código") 
-//O javaScript pode ser organizado em linhas (sentenças)
-
-{
-    console.log("Bloco de código");
-    //Também pode ser organizado em blocos de códigos.
-}
