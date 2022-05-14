06fa1dacd5979ca7e1a69ed0eb526a4fdb80c115
hudsonziin <hudsonteles00@gmail.com>
2022-05-14 01:13:09 -0300

    1att

---
+++ aulas_JS/organizacao.js
@@ -0,0 +1,7 @@
+console.log("Sentença de código") 
+//O javaScript pode ser organizado em linhas (sentenças)
+
+{
+    console.log("Bloco de código");
+    //Também pode ser organizado em blocos de códigos.
+}
